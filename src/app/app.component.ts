0fb6def8069bd6acbf942827dcfee6e41737fb52
same-hack <samehack.info@gmail.com>
2023-02-12 15:34:45 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MyDialogComponent } from './my-dialog/my-dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-material-confirm-dialog';
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    // ダイアログを開く※引数を渡すことも可
+    const dialogRef = this.dialog.open(MyDialogComponent, {
+      data: { text: 'データを更新' },
+    });
+
+    // ダイアログが閉じたら受け取る
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('ダイアログが閉じられました', result);
+      if (result) {
+        console.log('OKが押されました');
+      } else {
+        // resultはundefined
+        console.log('キャンセルが押されました');
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyDialogComponent } from './my-dialog/my-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MyDialogComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
